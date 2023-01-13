9449353c07cbd8582e06955a3a6042b676673a48
David J <davidhjolley2000@gmail.com>
2023-01-13 14:44:35 -0500

    Uploaded new logic
    Added a broken loop for the quiz, including a quiz object and function which should loop over the questions, it doesnt yet though and it crashed my chrome

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,33 @@
 var start = document.getElementById("start");
 var timerEl = document.getElementById('time');
+var questionTitle = document.getElementById('question-title');
 var endScreen = document.getElementById('end-screen');
+
+var questionsAndAnswers = {
+  "What is the capital of France?": "Paris",
+  "What is the largest planet in our solar system?": "Jupiter"
+};
 
 start.addEventListener("click", function(event) {
     var element = event.target;
   
     if(element.matches("button")){
       console.log("Quiz Started")//startquiz function
-      startQuiz()
+      startQuiz() // start the quiz
     }
   });
 
 
 function startQuiz(){
-    countdown()
+    quiz();
+    countdown();
 }
 
 function countdown() {
-  var timeLeft = 5;
-
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
+    var timeLeft = 5; // amount of time the player has
+    console.log("2");
     timeLeft--;
     timerEl.textContent = timeLeft;
 
@@ -28,6 +35,8 @@ function countdown() {
       clearInterval(timeInterval)
       gameOver();
     }
+
+    quiz();
 
   },1000);
 }
@@ -41,4 +50,23 @@ function gameOver(){
   {
     endScreen.classList.add('hide')
   }
+  return true;
+}
+
+function quiz(){
+  console.log("test");
+  var questions = Object.keys(questionsAndAnswers);
+  var answers = Object.values(questionsAndAnswers);
+  var currentQuestion = 0;
+  questionTitle.removeAttribute("data-state");
+  questionTitle.setAttribute("data-text", questions[currentQuestion]);
+
+  while (!gameOver()){
+    for (i = 0; i < questions.length; i++){
+      questionTitle.setAttribute("data-text", questions[currentQuestion]);
+      // if currentQuestion == the text inputted, go to next question (i++).
+    }
+  }
+  
+  // if currentQuestion == the text inputted, go to next question.
 }
