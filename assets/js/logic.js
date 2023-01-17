82ce69e81ce1208c1b6ee3de89fa8286bbaf769d
David J <davidhjolley2000@gmail.com>
2023-01-17 13:04:13 -0500

    Added a lot of updated logic
    - added a start button that when clicked, starts a timer and displays the first question.
    - added buttons for each answer, and when an answer is clicked, the next question appears.
    - added a penalty for incorrect answers where it subtracts time from the clock.
    - added a way to end the quiz when all questions are answered or the timer reaches 0.
    - added the ability for the user to save their initials and score when the game is over (NOT YET WORKING)

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,14 @@
 var start = document.getElementById("start");
 var timerEl = document.getElementById('time');
 var questionTitle = document.getElementById('question-title');
+var questionsDiv = document.getElementById('questions');
 var endScreen = document.getElementById('end-screen');
+var finalScore = document.getElementById('final-score');
+var submitBtn = document.getElementById('submit');
 
 var questionsAndAnswers = {
-  "What is the capital of France?": "Paris",
-  "What is the largest planet in our solar system?": "Jupiter"
+  "What is the capital of France?": ["Paris", "London", "Rome"],
+  "What is the largest planet in our solar system?": ["Jupiter", "Saturn", "Mars"]
 };
 
 start.addEventListener("click", function(event) {
@@ -17,6 +20,7 @@ start.addEventListener("click", function(event) {
     }
   });
 
+let timeLeft = 30;
 
 function startQuiz(){
     quiz();
@@ -25,9 +29,10 @@ function startQuiz(){
 
 function countdown() {
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  timeLeft = 30;
   var timeInterval = setInterval(function () {
-    var timeLeft = 5; // amount of time the player has
-    console.log("2");
+    
+    // amount of time the player has
     timeLeft--;
     timerEl.textContent = timeLeft;
 
@@ -36,9 +41,8 @@ function countdown() {
       gameOver();
     }
 
-    quiz();
-
   },1000);
+
 }
 
 
@@ -54,19 +58,52 @@ function gameOver(){
 }
 
 function quiz(){
-  console.log("test");
   var questions = Object.keys(questionsAndAnswers);
   var answers = Object.values(questionsAndAnswers);
   var currentQuestion = 0;
-  questionTitle.removeAttribute("data-state");
-  questionTitle.setAttribute("data-text", questions[currentQuestion]);
+  var score = 0;
+  questionsDiv.classList.remove('hide');
+  
+  displayQuestion();
 
-  while (!gameOver()){
-    for (i = 0; i < questions.length; i++){
-      questionTitle.setAttribute("data-text", questions[currentQuestion]);
-      // if currentQuestion == the text inputted, go to next question (i++).
+  function displayQuestion(){
+    questionTitle.textContent = questions[currentQuestion];
+    choices.innerHTML = "";
+    for (var i = 0; i < answers[currentQuestion].length; i++){
+      var choiceBtn = document.createElement("button");
+      choiceBtn.textContent = answers[currentQuestion][i];
+      choiceBtn.addEventListener("click", checkAnswer);
+      choices.appendChild(choiceBtn);
     }
   }
-  
-  // if currentQuestion == the text inputted, go to next question.
+
+  function checkAnswer(event){
+    var chosenAnswer = event.target.textContent;
+    if(chosenAnswer === answers[currentQuestion][0]){
+      currentQuestion++;
+      score++;
+      if(currentQuestion === questions.length){
+        gameOver();
+      } else {
+        displayQuestion();
+      }
+    } else {
+      timeLeft -= 10;
+      if(timeLeft <= 0){
+        gameOver();
+     }
+    }
+  }
+
+  function gameOver(){
+    questionsDiv.classList.add('hide');
+    endScreen.classList.remove('hide');
+    finalScore.textContent = score;
+    submitBtn.addEventListener("click", saveScore);
+  }
+
+  function saveScore(){
+    var initials = document.getElementById("initials").value;
+    // code to save initials and score to local storage or database
+  }
 }
